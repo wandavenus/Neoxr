013610bdb4f84c2b1684a0691047ad9e01db8ffa
wandavenus <75023651+wandavenus@users.noreply.github.com>
2023-02-19 14:48:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const {
 } = require(`./language`)
 
 //OpenAi || ChatGpt
-global.OpenAIKey = "YOUR_OPENAI_KEY" //login to https://beta.openai.com/ to get apikey
+global.OpenAIKey = "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" //login to https://beta.openai.com/ to get apikey
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
@@ -26,11 +26,11 @@ edit sewabot di file settings.js line 19
 global.firtsChat = true //send firts message to new user
 global.autoread = true // auto read message
 global.antidelete = false // anti delete
-global.antiviewonce = true // anti view once
-global.autobio = false //auto ganti bio
+global.antiviewonce = false // anti view once
+global.autobio = true //auto ganti bio
 global.anticall = true //anti call 
 global.userRegister = false
-global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
+global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
 //Available in indonesia , english & spanyol 
@@ -41,24 +41,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Neoxr-bot" //namabot kalian
+global.ownername = "Wndavenznchole" //nama kalian
+global.myweb = "-" //bebas asal jan hapus
+global.youtube = "https://instagram.com/wndavenznchole" //bebas asal jan hapus
+global.myweb2 = "https://instagram.com/wndavenznchole" //bebas
+global.email = "wandavenus25@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone list : https://www.php.net/manual/en/timezones.php 
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6283811412925'] //premium user
+global.owner = ["6283811412925", "62895411886929"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6283811412925" // nomor wa kalian
+global.ownernomerr = "+6283811412925" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = 'Created by Neoxr-bot' //sticker wm ubah
+global.author = '0895411886929' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 //type menu for menu
 global.typemenu = 'document' //
@@ -67,11 +67,11 @@ global.typemenu2 = 'templateImage' //
 global.multiplier = 69
 global.limitawal = {
    premium: "10000",
-   free: 100
+   free: 10
 }
 global.buttonDefault = [{
    urlButton: {
-      displayText: 'Youtube Channel',
+      displayText: 'Instagram',
       url: youtube
    }
 }, {
@@ -97,7 +97,7 @@ global.buttonDefault = [{
 }]
 global.buttonDefault2 = [{
    urlButton: {
-      displayText: 'Youtube Channel',
+      displayText: 'Instagram',
       url: youtube
    }
 }, {
@@ -139,13 +139,13 @@ global.buttonDefault3 = [{
 global.buttonDefault4 = [{
    buttonId: '.command',
    buttonText: {
-      displayText: '༺ Back'
+      displayText: 'Back'
    },
    type: 1
 }, {
    buttonId: '.owner',
    buttonText: {
-      displayText: 'Owner ༻'
+      displayText: 'Owner'
    },
    type: 1
 }]
@@ -212,7 +212,7 @@ global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
 global.prefa = ['', '!', '.', '🐦', '🐤', '🗿']
-global.sp = '⭔'
+global.sp = '•'
 global.mess = {
    sukses: '🤗Done, Oke Desu~',
    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
